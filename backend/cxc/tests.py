9b3033750645c3c8bb32b23fd2eed158d31e590b
Alexis Antonio Burgos Cante <95004883+alexisabc@users.noreply.github.com>
2025-07-29 14:21:12 -0500

    fix: add root view to avoid 404

--- backend/cxc/tests.py
+++ backend/cxc/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+
+class RootViewTests(APITestCase):
+    def test_root_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json().get("api"), "/api/")
