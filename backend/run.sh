e6d5c80cdc11b9938bdca24eb7f156bd6ee48f9c
Alexis Antonio Burgos Cante <95004883+alexisabc@users.noreply.github.com>
2025-08-05 18:02:30 -0500

    Call create_and_invite_superuser in startup script

--- backend/run.sh
+++ backend/run.sh
@@ -8,5 +8,8 @@ python manage.py collectstatic --noinput
 echo "Aplicando migraciones..."
 python manage.py migrate
 
+echo "Creando superusuario inicial..."
+python manage.py create_and_invite_superuser
+
 echo "Iniciando Gunicorn..."
 gunicorn luximia_erp.wsgi:application -b 0.0.0.0:10000
